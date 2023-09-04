ee0a3775f1566eebf571657fd23d0dfb1b7dd079
Lucas Dantas <lucaspvd@gmail.com>
2023-09-04 16:09:55 -0300

    Adicionados tipos_de_dados.py e variaveis_constantes.py

---
+++ tipos_de_dados.py
@@ -0,0 +1,5 @@
+print(1+10+559) #int= números inteiros#
+print(1.5+5.9) #float= números com ponto fluantes#
+print(True) #booleano ou bool= verdadeiro ou falso#
+print(False)
+print("Python") #str= sequencia de caracteres usando ("")#
---
+++ variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome = "Guilherme"
+idade = 28
+
+nome, idade = "Gi", 27
+print(nome, idade)
+
+limite_saque_diário = 1000 #usar padrão snake case para nomear, não usar abreviações.#
+
+BRAZILIAN_STATES = ["SP", "RJ"] #Por convensão ficou definido que quando houver uma variável com letras maiúsculas ela será uma constante imutável. é pra manter seu valor sem alteração, ou seja, o python deixa fazera alteração, mas o programador não deve alterar. #
+print(BRAZILIAN_STATES)
